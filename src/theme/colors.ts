3a7d1f4b382a945fefb5ff285331eaa0455a3135
Nelson Masbayi <nelsonmasbayi@gmail.com>
2025-01-17 11:06:36 +0300

    Setting up theme files

---
+++ src/theme/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    primary: '#22223B',     // Deep Purple
+    secondary: '#4A4E69',   // Indigo Purple
+    accent: '#9A8C98',      // Dusty Mauve
+    interactive: 'C9ADA7',  // Warm Rose
+    background: '#F2E9E4',  // Soft Cream
+};
